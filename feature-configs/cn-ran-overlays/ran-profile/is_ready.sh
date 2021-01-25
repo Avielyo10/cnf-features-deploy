cd8b330cb4cff4c78a81c033d7ca5488bf80a255
Aviel Yosef <Avielyo10@gmail.com>
2021-01-25 15:25:24 +0200

    Create is_ready.sh

---
+++ feature-configs/cn-ran-overlays/ran-profile/is_ready.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+WORKER_NODES=$(oc get nodes -l "node-role.kubernetes.io/worker-cnf" -o name)
+
+# Ensure numVfs is 4. If not, something is amiss
+for node in ${WORKER_NODES/node/sriovnetworknodestate}
+do
+  [[ $(oc -n openshift-sriov-network-operator get ${node} -o json \
+  | jq -r '.spec.interfaces[].numVfs | select(. != null)') -eq 4 ]] && \
+  [[ $(oc -n openshift-sriov-network-operator get ${node} -o json \
+  | jq -r '.status.interfaces[].numVfs | select(. != null)') -eq 4 ]]
+done
+
+oc wait ${CNF_MCP:-"mcp/worker-cnf"} --for condition=updated --timeout 1s
